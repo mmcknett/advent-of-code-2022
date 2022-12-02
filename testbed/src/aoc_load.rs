db6f5a2e665b53fa9db2a1a61bdfdb250a4b1934
Matt McKnett <mmcknett@gmail.com>
2022-12-01 17:49:25 -0800

    chore: Remove warnings from testbed

--- testbed/src/aoc_load.rs
+++ testbed/src/aoc_load.rs
@@ -4,16 +4,16 @@ use lalrpop_util::*;
 
 lalrpop_mod!(pub aoc);
 
-pub fn loadCommaSeparatedInts() -> Vec<i32> {
-  io::stdin().lock().lines().map(|line| parseCommaSeparated(&line.unwrap())).next().unwrap()
+pub fn load_comma_separated_ints() -> Vec<i32> {
+  io::stdin().lock().lines().map(|line| parse_comma_separated(&line.unwrap())).next().unwrap()
 }
 
-fn parseCommaSeparated(line: &str) -> Vec<i32> {
+fn parse_comma_separated(line: &str) -> Vec<i32> {
   let parser = aoc::CommaSepInt32sParser::new();
   parser.parse(line).unwrap()
 }
 
 #[test]
-fn testParseLine() {
-  assert_eq!(parseCommaSeparated("1,1,2,3,5,8,13"), vec![1,1,2,3,5,8,13]);
+fn test_parse_line() {
+  assert_eq!(parse_comma_separated("1,1,2,3,5,8,13"), vec![1,1,2,3,5,8,13]);
 }
--- testbed/src/main.rs
+++ testbed/src/main.rs
@@ -4,22 +4,18 @@ use utils;
 use lalrpop_util::*;
 
 mod ast;
-use ast::Expr::{self, *};
-use ast::OpCode::*;
 
 
 lalrpop_mod!(pub calculator);
 
-use calculator::{ExprParser, ExprsParser};
-
 mod aoc_load;
-use aoc_load::loadCommaSeparatedInts;
+use aoc_load::load_comma_separated_ints;
 
 use grid::*;
 
 fn main() {
     // e.g. call with `cat sample.txt | cargo run`
-    let ints = loadCommaSeparatedInts();
+    let ints = load_comma_separated_ints();
     println!("Loaded: {:?}", ints);
 
     // Same thing, but from utils
@@ -50,81 +46,92 @@ fn main() {
     println!("Grid is now: {:?}", g);
 }
 
-fn evaluate(exp: &Expr) -> i32 {
-    match exp {
-        N(x) => x.clone(),
-        Op(l, op, r) => {
-            let l_res = evaluate(l);
-            let r_res = evaluate(r);
-            match op {
-                Mul => l_res * r_res,
-                Div => l_res / r_res,
-                Add => l_res + r_res,
-                Sub => l_res - r_res,
+#[cfg(test)]
+mod test{
+    use super::*;
+
+    fn evaluate(exp: &Expr) -> i32 {
+        match exp {
+            N(x) => x.clone(),
+            Op(l, op, r) => {
+                let l_res = evaluate(l);
+                let r_res = evaluate(r);
+                match op {
+                    Mul => l_res * r_res,
+                    Div => l_res / r_res,
+                    Add => l_res + r_res,
+                    Sub => l_res - r_res,
+                }
             }
         }
     }
+    
+    fn evaluate_all(exps: &[Box<Expr>]) -> Vec<i32> {
+        exps.iter().map(|e| evaluate(e)).collect()
+    }
+
+    use calculator::{ExprsParser};
+    
+    #[test]
+    fn calculator_parens_valid() {
+        assert!(calculator::ExprParser::new().parse("22").is_ok());
+        assert!(calculator::ExprParser::new().parse("(22)").is_ok());
+        assert!(calculator::ExprParser::new().parse("((((22))))").is_ok());
+        assert!(calculator::ExprParser::new().parse("((22)").is_err());
+    }
+    
+    #[test]
+    fn figure_out_boxes() {
+        assert_eq!(*Box::new(32), 32);
+    }
+    
+    #[test]
+    fn structure() {
+        assert_eq!(&*(calculator::ExprParser::new().parse("2+2*6").unwrap()),
+            &Op(Box::new(N(2)), Add, Box::new(Op(Box::new(N(2)), Mul, Box::new(N(6))))));
+    
+        assert_eq!(&format!("{:?}", calculator::ExprParser::new().parse("2+2*6").unwrap()),
+            "Op(N(2), Add, Op(N(2), Mul, N(6)))");
+    
+        // From the tutorial code
+        let expr = calculator::ExprParser::new()
+            .parse("22 * 44 + 66")
+            .unwrap();
+        assert_eq!(&format!("{:?}", expr), "Op(Op(N(22), Mul, N(44)), Add, N(66))");
+    }
+
+    use ast::Expr::{self, *};
+    use ast::OpCode::*;
+
+    #[test]
+    fn calculations() {
+        assert_eq!(evaluate(&*calculator::ExprParser::new().parse("2+3").unwrap()), 5);
+        assert_eq!(evaluate(&*calculator::ExprParser::new().parse("2*6").unwrap()), 12);
+        assert_eq!(evaluate(&*calculator::ExprParser::new().parse("5+3*9-6/2").unwrap()), 29);
+        assert_eq!(evaluate(&*calculator::ExprParser::new().parse("(5+3)*9-6/2").unwrap()), 69);
+        assert_eq!(evaluate(&*calculator::ExprParser::new().parse("((2+3)*9-1)/2").unwrap()), 22);
+    }
+    
+    #[test]
+    fn ast_tests() {
+        assert_eq!(format!["{:?}", ast::OpCode::Mul], "Mul")
+    }
+    
+    #[test]
+    fn commas() {
+        assert_eq!(&format!("{:?}", ExprsParser::new().parse("2+2, 3*4").unwrap()), "[Op(N(2), Add, N(2)), Op(N(3), Mul, N(4))]");
+    
+        assert_eq!(evaluate_all(&*ExprsParser::new().parse("2+2,   3*4").unwrap()), [4, 12]);
+        assert_eq!(evaluate_all(&*ExprsParser::new().parse("2+2,   3*4,").unwrap()), [4, 12]);
+    }
+    
+    #[test]
+    fn errors() {
+        // Number is one bigger than std::i32::MAX
+        let expr = ExprsParser::new().parse("2147483648");
+        // assert_eq!(expr.err(), Some("number is too big"));
+        assert!(expr.is_err());
+        assert_eq!(expr.unwrap_err(), ParseError::User { error: "number is too big" });
+    }
 }
 
-fn evaluate_all(exps: &[Box<Expr>]) -> Vec<i32> {
-    exps.iter().map(|e| evaluate(e)).collect()
-}
-
-#[test]
-fn calculator_parens_valid() {
-    assert!(calculator::ExprParser::new().parse("22").is_ok());
-    assert!(calculator::ExprParser::new().parse("(22)").is_ok());
-    assert!(calculator::ExprParser::new().parse("((((22))))").is_ok());
-    assert!(calculator::ExprParser::new().parse("((22)").is_err());
-}
-
-#[test]
-fn figure_out_boxes() {
-    assert_eq!(*Box::new(32), 32);
-}
-
-#[test]
-fn structure() {
-    assert_eq!(&*(calculator::ExprParser::new().parse("2+2*6").unwrap()),
-        &Op(Box::new(N(2)), Add, Box::new(Op(Box::new(N(2)), Mul, Box::new(N(6))))));
-
-    assert_eq!(&format!("{:?}", calculator::ExprParser::new().parse("2+2*6").unwrap()),
-        "Op(N(2), Add, Op(N(2), Mul, N(6)))");
-
-    // From the tutorial code
-    let expr = calculator::ExprParser::new()
-        .parse("22 * 44 + 66")
-        .unwrap();
-    assert_eq!(&format!("{:?}", expr), "Op(Op(N(22), Mul, N(44)), Add, N(66))");
-}
-
-#[test]
-fn calculations() {
-    assert_eq!(evaluate(&*calculator::ExprParser::new().parse("2+3").unwrap()), 5);
-    assert_eq!(evaluate(&*calculator::ExprParser::new().parse("2*6").unwrap()), 12);
-    assert_eq!(evaluate(&*calculator::ExprParser::new().parse("5+3*9-6/2").unwrap()), 29);
-    assert_eq!(evaluate(&*calculator::ExprParser::new().parse("(5+3)*9-6/2").unwrap()), 69);
-    assert_eq!(evaluate(&*calculator::ExprParser::new().parse("((2+3)*9-1)/2").unwrap()), 22);
-}
-
-#[test]
-fn ast_tests() {
-    assert_eq!(format!["{:?}", ast::OpCode::Mul], "Mul")
-}
-
-#[test]
-fn commas() {
-    assert_eq!(&format!("{:?}", ExprsParser::new().parse("2+2, 3*4").unwrap()), "[Op(N(2), Add, N(2)), Op(N(3), Mul, N(4))]");
-
-    assert_eq!(evaluate_all(&*ExprsParser::new().parse("2+2,   3*4").unwrap()), [4, 12]);
-    assert_eq!(evaluate_all(&*ExprsParser::new().parse("2+2,   3*4,").unwrap()), [4, 12]);
-}
-
-#[test]
-fn errors() {
-    // Number is one bigger than std::i32::MAX
-    let expr = ExprsParser::new().parse("2147483648");
-    // assert_eq!(expr.err(), Some("number is too big"));
-    assert!(expr.is_err());
-    assert_eq!(expr.unwrap_err(), ParseError::User { error: "number is too big" });
-}
