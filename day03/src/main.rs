7cd1bd21d0b19de5a07c28031a6112e6662b25bd
Matt McKnett <mmcknett@gmail.com>
2022-12-02 22:08:09 -0800

    Day 3 part 2 complete

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,9 +1,10 @@
-use std::convert;
+use itertools::Itertools;
 
-use regex::Regex;
+use std::collections::HashSet;
 
 // Priority
-type pri = u32;
+type Pri = u32;
+
 
 fn main() {
     // Read in the file provided as the first argument.
@@ -14,23 +15,46 @@ fn main() {
     // For this one, read a string of lower and upper-case chars.
     // Convert these into 1-26 (a-z) and 27-52 (A-Z)
     // Then split the list into two
-    let common_pris: Vec<pri> = input.split("\n").map(|s: &str| find_common_item(load_string_to_two_vecs(s))).collect();
+    let common_pris: Vec<Pri> = input.split("\n").map(|s: &str| find_common_item(load_string_to_two_vecs(s))).collect();
 
     // Part 1
     let sum_of_commons: u32 = common_pris.iter().sum();
     println!("The sum of priorities of types that are in both compartments is {sum_of_commons}.");
 
     // Part 2
+    // Re-parse the input to get groups of three lines. Find the common number among the three lines.
+    let badge_sum: u32 = input.split("\n").chunks(3).into_iter().map(
+        |group| {
+            let elves: Vec<HashSet<Pri>> = group.map(|s: &str| s.chars().map(char_to_pri).collect()).into_iter().collect();
+            let badge: Pri = intersect(elves);
+            return badge;
+        }
+    ).sum();
+    println!("The sum of badge priorities for each group of three is {badge_sum}");
 }
 
-fn load_string_to_two_vecs(input: &str) -> (Vec<pri>, Vec<pri>) {
+fn intersect(pri_sets: Vec<HashSet<Pri>>) -> Pri {
+    // Find the unique intersection of each set.
+    let intersection: HashSet<Pri> = pri_sets.iter().fold(
+        HashSet::new(),
+        |intersection: HashSet<Pri>, next_set: &HashSet<Pri>|
+            if intersection.is_empty() { 
+                next_set.clone()
+            } else {
+                intersection.intersection(next_set).copied().collect()
+            }
+    );
+    return intersection.iter().next().copied().expect("The intersection was empty!");
+}
+
+fn load_string_to_two_vecs(input: &str) -> (Vec<Pri>, Vec<Pri>) {
     let iter = input.chars();
-    let first: Vec<pri> = iter.clone().take(input.len() / 2).map(char_to_pri).collect();
-    let second: Vec<pri> = iter.skip(input.len() / 2).map(char_to_pri).collect();
+    let first: Vec<Pri> = iter.clone().take(input.len() / 2).map(char_to_pri).collect();
+    let second: Vec<Pri> = iter.skip(input.len() / 2).map(char_to_pri).collect();
     return (first, second)
 }
 
-fn find_common_item((first, second): (Vec<pri>, Vec<pri>)) -> pri {
+fn find_common_item((first, second): (Vec<Pri>, Vec<Pri>)) -> Pri {
     for f in first {
         match second.iter().copied().find(|s| f == *s) {
             Some(s) => return s,
@@ -40,7 +64,7 @@ fn find_common_item((first, second): (Vec<pri>, Vec<pri>)) -> pri {
     panic!("Didn't find a common item!");
 }
 
-fn char_to_pri(c: char) -> pri {
+fn char_to_pri(c: char) -> Pri {
     if c.is_lowercase() {
         c as u32 - 96  // 'a' is 97, should be 1
     } else {
@@ -78,5 +102,10 @@ mod tests {
     #[test]
     fn find_common() {
         assert_eq!(find_common_item((vec![1,3,5], vec![6,9,3])), 3);
+    }
+
+    #[test]
+    fn intersection_test() {
+        assert_eq!(intersect(vec![HashSet::from([1,2,3]), HashSet::from([4,3,2]), HashSet::from([1,2,5])]), 2);
     }
 }
