cfb67c0bc9ad244504169b503dddc55a05b396e1
Matt McKnett <mmcknett@gmail.com>
2022-12-10 00:07:05 -0800

    feat: Day 10 part 1 complete

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,11 +1,36 @@
+mod processor;
+use processor::{Processor, Instruction};
+
 fn main() {
     // Read in the file provided as the first argument.
     let path = utils::args_iter().next().expect("Missing argument");
     let input = std::fs::read_to_string(&path).expect(&format!["Couldn't find file \"{path}\""]);
 
     // Parse input
+    let instructions: Vec<Instruction> = input.split("\n").map(|s| s.parse::<Instruction>().unwrap()).collect();
+    let mut processor = Processor::new(instructions);
 
     // Part 1
+    for _ in 0..19 { // The prompt is to get X *during* the tick, not *after* the tick.
+        processor.tick();
+    }
+    let strength_on_20 = processor.signal_strength();
+    let mut signal_strengths = strength_on_20;
+    println!("Signal strength on tick {} (x={}) is {}", processor.clock, processor.x, strength_on_20);
+
+    for _ in 0..5 {
+        for _ in 0..40 {
+            processor.tick();
+            if processor.clock > 180 {
+                // println!("Signal strength on tick {} (x={}) is {}", processor.clock, processor.x, processor.signal_strength());
+            }
+        }
+        let next_40 = processor.signal_strength();
+        println!("Signal strength on tick {} (x={}) is {}", processor.clock, processor.x, next_40);
+        signal_strengths += next_40;
+    }
+
+    println!("Sum of signal strengths is {signal_strengths}");
 
     // Part 2
 }
---
+++ day10/src/processor.rs
@@ -0,0 +1,121 @@
+use std::str::FromStr;
+
+#[derive(Debug, PartialEq)]
+pub enum Instruction {
+  Add(i32),
+  Noop
+}
+
+impl FromStr for Instruction {
+  type Err = ();
+
+  fn from_str(input: &str) -> Result<Self, Self::Err> {
+    let tokens: Vec<&str> = input.split(" ").collect();
+    match tokens[0] {
+      "noop" => Ok(Instruction::Noop),
+      "addx" => Ok(Instruction::Add(tokens[1].parse::<i32>().unwrap())),
+      _ => Err(())
+    }
+  }
+}
+
+pub struct Processor {
+  pub x: i32,
+  pub clock: u64,
+  program_iter: std::vec::IntoIter<Instruction>,
+  stalled_instruction: Option<Instruction>,
+  instruction_stall: u8
+}
+
+impl Processor {
+  pub fn new(program: Vec<Instruction>) -> Self {
+    Self {
+      x: 1,
+      clock: 1, // To account for the "during" logic, start at 1 instead of 0.
+      program_iter: program.into_iter(),
+      stalled_instruction: None,
+      instruction_stall: 0
+    }
+  }
+
+  pub fn tick(&mut self) {
+    self.clock += 1;
+
+    if self.stalled_instruction.is_some() {
+      // There's a stalled instruction. Decrement the stall and finish the operation if needed.
+      self.tick_stalled();
+      return;
+    }
+
+    // Do next instruction or start the stall.
+    let next_inst = self.program_iter.next();
+    match next_inst {
+      Some(Instruction::Add(x)) => {
+        self.stalled_instruction = next_inst;
+        self.instruction_stall = 1;
+        // println!("Stalling Add({x})");
+      },
+      Some(Instruction::Noop) => (), // println!("Noop"),
+      _ => self.instruction_stall = 0,
+    }
+  }
+
+  fn tick_stalled(&mut self) {
+    self.instruction_stall -= 1;
+
+    // Finish operation if the stall is done.
+    if self.instruction_stall == 0 {
+      match self.stalled_instruction {
+        Some(Instruction::Add(a)) => {
+          self.x += a;
+          // println!("Finishing Add({a})");
+        }
+        _ => ()
+      }
+      self.stalled_instruction = None;
+    }
+  }
+
+  pub fn signal_strength(&self) -> i32 {
+    self.clock as i32 * self.x
+  }
+}
+
+#[cfg(test)]
+mod processor_tests {
+  use super::*;
+
+  #[test]
+  fn instruction_parsing() {
+    let i = "addx -15".parse::<Instruction>();
+    assert_eq!(i, Ok(Instruction::Add(-15)));
+
+    let i = "noop".parse::<Instruction>();
+    assert_eq!(i, Ok(Instruction::Noop));
+
+    let i = "blahblah".parse::<Instruction>();
+    assert_eq!(i, Err(()));
+  }
+
+  #[test]
+  fn small_program() {
+    let program = vec![
+      Instruction::Noop,
+      Instruction::Add(3),
+      Instruction::Add(-5)
+    ];
+    let mut proc = Processor::new(program);
+
+    assert_eq!(proc.x, 1);
+    proc.tick(); // Noop
+    assert_eq!(proc.x, 1);
+    proc.tick(); // Add 3 stalled
+    assert_eq!(proc.x, 1);
+    proc.tick(); // Add 3 finished
+    assert_eq!(proc.x, 4);
+    proc.tick(); // Add -5 stalled
+    assert_eq!(proc.x, 4);
+    proc.tick(); // Add -5 finished
+    assert_eq!(proc.x, -1);
+  }
+}
