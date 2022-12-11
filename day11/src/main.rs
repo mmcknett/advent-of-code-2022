0df3d803302fe80bbfe885b5204a1a6b5a9b18f3
Matt McKnett <mmcknett@gmail.com>
2022-12-10 22:00:29 -0800

    feat: Day 11 Part 1 complete

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 fn main() {
     // Read in the file provided as the first argument.
     let path = utils::args_iter().next().expect("Missing argument");
@@ -8,7 +10,7 @@ fn main() {
     let mut monkeys = vec![];
     for block in monkey_blocks {
         let mut lines = block.split("\n").skip(1);
-        let items: Vec<i32> = lines.next().unwrap()
+        let items: VecDeque<i32> = lines.next().unwrap()
             .split("Starting items: ").skip(1).next().unwrap()
             .split(", ").map(|s| s.parse::<i32>().unwrap()).collect();
         let operation: Vec<&str> = lines.next().unwrap()
@@ -18,13 +20,73 @@ fn main() {
         let divisor = lines.next().unwrap().split(" by ").skip(1).next().unwrap().parse::<i32>().unwrap();
         let monkey_idx_if_true = lines.next().unwrap().split(" ").last().unwrap().parse::<usize>().unwrap();
         let monkey_idx_if_false = lines.next().unwrap().split(" ").last().unwrap().parse::<usize>().unwrap();
-        monkeys.push(Monkey { items, operation, divisor, monkey_idx_if_true, monkey_idx_if_false });
+        monkeys.push(Monkey { items, operation, divisor, monkey_idx_if_true, monkey_idx_if_false, inspect_count: 0 });
     }
 
     // Part 1
-    println!("Monkeys are {:?}", monkeys);
+    println!("Monkeys are...");
+    for m in &monkeys {
+        println!("{:?}", m);
+    }
+
+    for _ in 0..20 {
+        play_round(&mut monkeys);
+    }
+
+    println!("After round 20, monkeys are...");
+    for m in &monkeys {
+        println!("{:?}", m.items);
+    }
+    println!("Inspections...");
+    for m in &monkeys {
+        println!("{:?}", m.inspect_count);
+    }
+
+    let mut monkey_business: Vec<u32> = monkeys.iter().map(|m| m.inspect_count).collect();
+    monkey_business.sort();
+    monkey_business.reverse();
+    let monkey_business = monkey_business[0] * monkey_business[1];
+    println!("Monkey business is {monkey_business}");
 
     // Part 2
+}
+
+fn play_round(monkeys: &mut Vec<Monkey>) {
+    for i in 0..monkeys.len() {
+        while let Some(item) = monkeys[i].items.pop_front() {
+            // Inspect an item -- relief makes value be floor-divided by 3.
+            monkeys[i].inspect_count += 1;
+            let worry_level = calc_new_worry(item, &monkeys[i].operation) / 3;
+
+            // Monkey tests worry level
+            if worry_level % monkeys[i].divisor == 0 {
+                let next_monkey = monkeys[i].monkey_idx_if_true;
+                monkeys[next_monkey].items.push_back(worry_level);
+            } else {
+                let next_monkey = monkeys[i].monkey_idx_if_false;
+                monkeys[next_monkey].items.push_back(worry_level);
+            }
+        }
+    }
+}
+
+fn calc_new_worry(old: i32, ops: &Vec<Op>) -> i32 {
+    let l = match ops[0] {
+        Op::Old => old,
+        Op::Num(x) => x,
+        _ => panic!("Invalid left")
+    };
+    let r = match ops[2] {
+        Op::Old => old,
+        Op::Num(x) => x,
+        _ => panic!("Invalid right")
+    };
+
+    match ops[1] {
+        Op::Plus => l + r,
+        Op::Times => l * r,
+        _ => panic!("Invalid expression")
+    }
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -53,11 +115,12 @@ impl Op {
 
 #[derive(Debug, PartialEq, Eq)]
 struct Monkey {
-    items: Vec<i32>,
+    items: VecDeque<i32>,
     operation: Vec<Op>,
     divisor: i32,
     monkey_idx_if_true: usize,
-    monkey_idx_if_false: usize
+    monkey_idx_if_false: usize,
+    inspect_count: u32
 }
 
 #[cfg(test)]
