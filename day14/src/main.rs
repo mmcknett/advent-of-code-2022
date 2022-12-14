c2f2d0c440b70f1618f30c7cb4c86a91eadb0ed5
Matt McKnett <mmcknett@gmail.com>
2022-12-13 21:56:33 -0800

    feat: Day 14 part 1 complete

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,3 +1,6 @@
+use std::cmp::{min,max};
+use std::collections::HashSet;
+
 use utils::load::coord_2d_parser::PolyParser;
 use utils::coordinates::Coord;
 
@@ -12,10 +15,68 @@ fn main() {
     let bottom: i32 = polys.iter().flatten().map(|c| c.y).max().unwrap();
 
     // Part 1
-    let num_sands = 0;
+    let mut walls = get_walls(&polys);
+    let mut num_sands = 0;
+    while drop_one_sand(&mut walls, bottom) {
+        num_sands += 1;
+    }
     println!("Part 1: Number of sand units that come to rest is {num_sands}");
 
     // Part 2
+}
+
+fn get_walls(polys: &Vec<Vec<Coord>>) -> HashSet<Coord> {
+    let mut walls: HashSet<Coord> = HashSet::new();
+
+    for poly in polys {
+        for window in poly.windows(2) {
+            let cs = window[0];
+            let ce = window[1];
+            if cs.x == ce.x {
+                // Add every point in between cs and ce varying y
+                for y in min(cs.y, ce.y)..=max(cs.y, ce.y) {
+                    walls.insert(Coord::new2(cs.x, y));
+                }
+            } else if cs.y == ce.y {
+                // Add every point in between cs and ce varying x
+                for x in min(cs.x, ce.x)..=max(cs.x, ce.x) {
+                    walls.insert(Coord::new2(x, cs.y));
+                }
+            } else {
+                panic!("No diagonals!");
+            }
+        }
+    }
+
+    return walls;
+}
+
+// Return true if the sand stops, false if it fell forever.
+fn drop_one_sand(mut walls: &mut HashSet<Coord>, bottom: i32) -> bool {
+    let mut sand = Coord::new2(500,0);
+
+    'outer: while sand.y <= bottom {
+        let dirs = [
+            Coord::new2(sand.x, sand.y+1),   // down
+            Coord::new2(sand.x-1, sand.y+1), // down, left
+            Coord::new2(sand.x+1, sand.y+1)  // down, right
+        ];
+        for direction in dirs {
+            if walls.contains(&direction) {
+                continue; // Try the next direction.
+            } else {
+                sand = direction; // This direction isn't blocked; go that way.
+                continue 'outer;
+            }
+        }
+
+        // Sand was fully blocked and comes to rest.
+        walls.insert(sand);
+        return true;
+    }
+
+    // Sand passed the bottom-most coordinate, so it will fall forever.
+    return false;
 }
 
 #[cfg(test)]
@@ -24,6 +85,6 @@ mod tests {
 
     #[test]
     fn day_14_test() {
-        assert_eq![1, 1]
+        
     }
 }
