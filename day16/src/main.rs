3dd736b86fa2a538f2a41991acd0965f8082d37b
Matt McKnett <mmcknett@gmail.com>
2022-12-16 09:59:20 -0800

    feat: Day 16 Part 1 complete...ish
    I left this running overnight and it produced the correct answer, but that's obviously not acceptable performance.

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,9 +1,22 @@
 #[macro_use] extern crate scan_fmt;
 use core::time;
-use std::collections::{HashMap, HashSet};
+use std::{collections::{HashMap, HashSet}, hash::Hash};
 
-type Valve = String;
 type Tunnels = HashMap<Valve, Vec<Valve>>;
+
+#[derive(Hash, PartialEq, Eq, PartialOrd, Ord, Debug, Clone, Copy)]
+struct Valve {
+    val: [char; 2]
+}
+
+impl Valve {
+    fn from(s: &str) -> Self {
+        let mut iter = s.chars();
+        Self {
+            val: [iter.next().unwrap(), iter.next().unwrap()]
+        }
+    }
+}
 
 fn main() {
     // Read in the file provided as the first argument.
@@ -11,14 +24,14 @@ fn main() {
     let input = std::fs::read_to_string(&path).expect(&format!["Couldn't find file \"{path}\""]);
 
     // Parse input
-    let instructions: Vec<(Valve, u32, String)> = input
+    let instructions: Vec<(String, u32, String)> = input
         .split("\n")
         .map(|line| scan_fmt![line, "Valve {} has flow rate={d}; {*/tunnels lead|tunnel leads/} to valve{*/s?/} {/.*/}", String, u32, String].unwrap())
         .collect();
 
     let mut tunnels: Tunnels = instructions.iter()
         .map(
-            |ins| (ins.0.clone(), ins.2.split(", ").map(|s| s.to_string()).collect())
+            |ins| (Valve::from(&ins.0), ins.2.split(", ").map(|s| Valve::from(s)).collect())
         ).collect();
 
     // Part 1
@@ -27,12 +40,12 @@ fn main() {
     // Keep finding the next-maximal valve.
     let positive_valves: HashMap<Valve, u32> = instructions.iter().filter_map(
         |ins|
-            if ins.1 > 0 { Some((ins.0.clone(), ins.1)) } else { None }
+            if ins.1 > 0 { Some((Valve::from(&ins.0), ins.1)) } else { None }
         ).collect();
-    let start: Valve = "AA".to_string();
-    let minutes_remain: u32 = 17; //30;
+    let start: Valve = Valve::from("AA");
+    let minutes_remain: u32 = 30;
 
-    let pressure_released = find_max_release(&start, &tunnels, &positive_valves, minutes_remain, 0 /* current_release */);
+    let pressure_released = find_max_release(&start, &tunnels, &positive_valves, minutes_remain, 0 /* current_release */, &mut HashMap::new());
 
     println!("Max pressure release is: {pressure_released}");
     // Tried 1021, but that's too low. <-- fixed the algorithm
@@ -41,7 +54,14 @@ fn main() {
     // Part 2
 }
 
-fn find_max_release(curr: &Valve, tunnels: &Tunnels, positive_valves: &HashMap<Valve, u32>, time_remain: u32, current_release: u32) -> u32 {
+fn find_max_release(
+    curr: &Valve,
+    tunnels: &Tunnels,
+    positive_valves: &HashMap<Valve, u32>,
+    time_remain: u32,
+    current_release: u32,
+    mut param_cache: &mut HashMap<(Valve, Vec<Valve>, u32, u32), u32>) -> u32
+{
     // println!("Visiting {curr} with {time_remain} remaining");
     if time_remain == 0 {
         // println!("Out of time");
@@ -53,6 +73,17 @@ fn find_max_release(curr: &Valve, tunnels: &Tunnels, positive_valves: &HashMap<V
         let result = time_remain * current_release;
         // println!("Shortcutting at {time_remain}, max is {result}");
         return result;
+    }
+
+    let params = (
+        curr.clone(),
+        positive_valves.iter().map(|(v, _)| v.clone()).collect::<Vec<Valve>>(),
+        time_remain,
+        current_release
+    );
+
+    if let Some(result) = param_cache.get(&params) {
+        return *result;
     }
 
     // Try releasing this valve. Drop the current valve from positive valves.
@@ -67,17 +98,20 @@ fn find_max_release(curr: &Valve, tunnels: &Tunnels, positive_valves: &HashMap<V
                 |pair| ((*pair.0).clone(), *pair.1)
             ).collect(), // Copy the valves w/ values
             time_remain - 1,
-            current_release + c)
+            current_release + c,
+            param_cache
+        )
     };
 
     // Try going down all the connecting tunnels instead.
     let max_connection = tunnels.get(curr).unwrap().iter().map(
         |connection|
-            find_max_release(connection, tunnels, positive_valves, time_remain - 1, current_release)
+            find_max_release(connection, tunnels, positive_valves, time_remain - 1, current_release, param_cache)
     ).max().unwrap();
 
     let result = current_release + std::cmp::max(max_release_this, max_connection);
     // println!("From {curr} with {time_remain} remaining, max was {result}");
+    param_cache.insert(params, result);
     return result;
 }
 
