f5f6805156ea68a9156ba98d0bd4a692963aa364
Matt McKnett <mmcknett@gmail.com>
2023-01-09 08:40:57 -0800

    feat: while sample has unique numbers, input does *not*.

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,11 +1,18 @@
+use std::collections::HashSet;
+
 fn main() {
     // Read in the file provided as the first argument.
     let path = utils::args_iter().next().expect("Missing argument");
     let input = std::fs::read_to_string(&path).expect(&format!["Couldn't find file \"{path}\""]);
 
     // Parse input
+    let numbers: Vec<isize> = input.split("\n").map(|n| n.parse::<isize>().unwrap()).collect();
 
     // Part 1
+    let mut mixed = numbers.clone();
+    let set: HashSet<isize> = mixed.iter().cloned().collect();
+
+    println!("Numbers are unique: {}", set.len() == mixed.len())
 
     // Part 2
 }
