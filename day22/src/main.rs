84c6671420f49efcc18b43604affb33f2f2f07d5
Matt McKnett <mmcknett@gmail.com>
2023-01-16 19:19:04 -0800

    chore: Day 22 begin

---
+++ day22/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    // Read in the file provided as the first argument.
+    let path = utils::args_iter().next().expect("Missing argument");
+    let input = std::fs::read_to_string(&path).expect(&format!["Couldn't find file \"{path}\""]);
+
+    // Parse input
+
+    // Part 1
+
+    // Part 2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn day_22_test() {
+        assert_eq![1, 1]
+    }
+}
