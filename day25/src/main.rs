3df242fb88ae3966c524509bab1732b434bd4326
Matt McKnett <mmcknett@gmail.com>
2023-01-24 17:01:36 -0800

    feat: Day 25 parsing complete

--- day25/src/main.rs
+++ day25/src/main.rs
@@ -12,7 +12,8 @@ fn main() {
 
 fn parse(path: &str) -> Vec<String> {
     let input = std::fs::read_to_string(&path).expect(&format!["Couldn't find file \"{path}\""]);
-    let res: Vec<String> = input.split("\n").iter().map(|s| String::from(s)).collect();
+    let res: Vec<String> = input.split("\n").into_iter().map(|s| String::from(s)).collect();
+    return res;
 }
 
 fn snafu(input: &str) -> i64 {
@@ -25,6 +26,23 @@ mod tests {
 
     #[test]
     fn day_25_test() {
-        
+        let nums = parse("sample.txt");
+        let dec_nums: Vec<i64> = nums.into_iter().map(|s| snafu(&s)).collect();
+        assert_eq!(dec_nums,
+        [
+            1747,
+            906,
+            198,
+            11,
+            201,
+            31,
+            1257,
+            32,
+            353,
+            107,
+            7,
+            3,
+            37,
+        ])
     }
 }
